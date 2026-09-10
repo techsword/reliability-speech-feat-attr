25bf66f590d4407ba0f396692bc2d47f4b28fd7d
Gaofei Shen <gaofei.shen@techsword.me>
2026-09-10 11:06:29 +0200

    Ignore generated artifacts and read SPEAKERS.TXT from data/

--- src/preprocessing/dataset_gender.py
+++ src/preprocessing/dataset_gender.py
@@ -47,7 +47,7 @@ def load_librispeech_as_gender_classification_dataset(
 
     # Loading speaker information
     speaker_metainfo = pd.read_csv(
-        os.path.join(PROJECT_ROOT, "SPEAKERS.TXT"),
+        os.path.join(PROJECT_ROOT, "..", "data", "SPEAKERS.TXT"),
         sep="|",
         skiprows=12,
         skipinitialspace=True,
